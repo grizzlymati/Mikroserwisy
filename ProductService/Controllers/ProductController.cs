47b4aacb42b26bce97e53ff9c79f070878a1dddd
Mateusz <matik788@gmail.com>
2019-08-27 00:01:45 +0200

    poprawienie api productService oraz routingu

--- ProductService/Controllers/ProductController.cs
+++ ProductService/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using ProductService.Repository;
 using Microsoft.AspNetCore.Mvc;
 using System.Transactions;
+using System.Collections.Generic;
 
 namespace ProductService.Controllers
 {
@@ -17,71 +18,30 @@ namespace ProductService.Controllers
             _productRepository = productRepository;
         }
 
-        [HttpGet("oldest")]
-        public IActionResult Get()
+        [HttpGet("GetAllProducts")]
+        public IActionResult GetAllProducts()
         {
-            var products = _productRepository.GetProducts();
+            IEnumerable<Product> products = _productRepository.GetProducts();
             return new OkObjectResult(products);
         }
 
-        [HttpGet("latest")]
-        public IActionResult Get2()
-        {
-            var products = _productRepository.GetProducts();
-            return new OkObjectResult(products);
-        }
-
-
-        // GET: api/Product/5
-        [HttpGet("/get2/{id}")]
-        public IActionResult Get(int id)
-        {
-            var product = _productRepository.GetProductByID(id);
-            return new OkObjectResult(product);
-        }
-
-        // POST: api/Product
-        [HttpPost("/post1")]
-        public IActionResult Post([FromBody] Product product)
+        [HttpPost("CreateProduct")]
+        public IActionResult CreateProduct([FromBody] Product product)
         {
             using (var scope = new TransactionScope())
             {
                 _productRepository.InsertProduct(product);
                 scope.Complete();
-                return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+                return Ok(product);
             }
         }
 
-        // PUT: api/Product/5
-        [HttpPut("/put1")]
-        public IActionResult Put([FromBody] Product product)
-        {
-            if (product != null)
-            {
-                using (var scope = new TransactionScope())
-                {
-                    // _productRepository.UpdateProduct(product);
-                    scope.Complete();
-                    return new OkResult();
-                }
-            }
-            return new NoContentResult();
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("/delete1/{id}")]
-        public IActionResult Delete(int id)
+        [HttpDelete("DeleteProduct")]
+        public IActionResult DeleteProduct(int id)
         {
             _productRepository.DeleteProduct(id);
             return new OkResult();
         }
 
-
-        [HttpDelete("/delete2/{idik}")]
-        public IActionResult Delete3(int idik)
-        {
-            _productRepository.DeleteProduct(2);
-            return new OkResult();
-        }
     }
 }
