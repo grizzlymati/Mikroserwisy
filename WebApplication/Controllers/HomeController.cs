15eec871899a93b2b672e8244c22b54acaff56d0
Mateusz <matik788@gmail.com>
2019-08-31 20:37:59 +0200

    dodanie czesci front-endowej wraz z wstepna implementwacja pierwszego widoku produktow - dodanie grida wyswietlajacego produktu - dodatnie funkcjonalnosci wybierania ilosci produktow jak i zgloszenia zamowienia (mockowane dane, brak polaczenia z serwisami)

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult UserPanel(AuthModel authModel)
+        {
+            string userData = authModel.GetHashCode().ToString();
+            //return RedirectToAction(nameof(ProductsController.Index), "Products", new { id = userData });
+            return View();
+        }
+
+        public JsonResult GetAllProducts()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product()
+                {
+                    Id = 14,
+                    Name = "Test1",
+                    Description = "DescTest1",
+                    Price = 2.1M,
+                    Amount = 4
+                },
+                   new Product()
+                {
+                    Id = 11,
+                    Name = "Test12",
+                    Description = "DescTest21",
+                    Price = 2.11M,
+                    Amount = 5
+                }
+            };
+            return new JsonResult(products);
+        }
+
+        public IActionResult SendOrder(ProductOrderDetails[] orderData)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication/Models/ProductOrderDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication.Models
+{
+    public class ProductOrderDetails
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+    }
+}
